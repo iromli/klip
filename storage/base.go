e4778118c26ca550e609096d5cb7766019fe4116
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-12-21 06:01:46 +0700

    satisfy golint

--- storage/base.go
+++ storage/base.go
@@ -3,11 +3,15 @@ package storage
 import "errors"
 
 var (
+	// ErrMissingItem represents error about missing list item
 	ErrMissingItem = errors.New("item not found")
+
+	// ErrMissingList represents error about missing list
 	ErrMissingList = errors.New("list not found")
 )
 
-type StorageManager interface {
+// Manager is an interface for a storage.
+type Manager interface {
 	Put(list, name, value string) error
 	Get(list, name string) (string, error)
 	Map(list string) (map[string]interface{}, error)
