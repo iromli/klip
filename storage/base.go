12649b772505619da16df9bbeb23fb70135e3273
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-01-03 04:24:38 +0700

    remove unused Manager interface

--- storage/base.go
+++ storage/base.go
@@ -9,11 +9,3 @@ var (
 	// ErrMissingList represents error about missing list
 	ErrMissingList = errors.New("list not found")
 )
-
-// Manager is an interface for a storage.
-type Manager interface {
-	Put(list, name string, value interface{}) error
-	Get(list, name string) (string, error)
-	Map(list string) (map[string]interface{}, error)
-	Delete(list, name string) error
-}
